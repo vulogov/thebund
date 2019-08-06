17dc899f374fa2e223ac372f241ac73ead088d8e
Vladimir Ulogov <vulogov@linkedin.com>
2019-08-06 12:01:07 -0700

    Initial submission

---
+++ src/bund/bund_grammar.py
@@ -0,0 +1,150 @@
+bund_grammar="""
+BundContexts:
+  history*=HistoryElement
+  env*=EnvironmentElement
+  contexts+=Context
+;
+
+HistoryElement:
+  '[history'
+    story*=HistoryElementKV
+  ';;'
+;
+
+HistoryElementKV:
+  ID 'is' BASETYPE
+;
+
+EnvironmentElement:
+  '[env'
+    envstatement += EnvStatement
+  ';;'
+;
+
+EnvStatement:
+  name=ID "is" type=ID("[" param=BASETYPE "]")?
+;
+
+Context:
+  name=ID 'is'
+    statements+=Statement
+  ';;'
+;
+
+Statement:
+  DataBlock | CodeBlockDecl | InBlockDecl | OutBlockDecl | VarBlockDecl
+;
+
+DataBlock:
+  '[data'
+    definitions+=DataDefinitions[/,|;|(\s)*|(\n)*/]
+    codeblock=CodeBlock?
+  ';;'
+;
+
+CodeBlockDecl:
+  name=ID 'is' codeblock=CodeBlock
+;
+
+InBlockDecl:
+  '[in'
+    in_chan += ChannelDefs
+  ';;'
+;
+
+OutBlockDecl:
+  '[out'
+    out_chan += ChannelDefs
+  ';;'
+;
+
+ChannelDefs:
+  name=ID "is" type=ChannelType
+    attrs *= ChannelAttrs[/,|;|(\s)*|(\n)*/]
+  ";;"
+;
+
+ChannelType:
+  "file" | "pipe"
+;
+
+ChannelAttrs:
+  name=ID '<-' val=BASETYPE
+;
+
+VarBlockDecl:
+  '[var'
+    PushAssignment | PullAssignment
+  ';;'
+;
+
+DataDefinitions:
+  PushAssignment | PullAssignment
+;
+
+PushAssignment:
+  value=Data '->' name=ID
+;
+
+PullAssignment:
+  name=ID '<-' value=Data
+;
+
+List:
+  '[' value+=Data[','] ']'
+;
+
+KVData:
+  name=ID '<-' Data
+;
+
+KV:
+  '{' kvalue+=KVData[',']
+  '}'
+;
+
+Data:
+  BASETYPE | List | KV | CodeBlock | CodeBlockRef
+;
+
+CodeBlock:
+  "(" words+=CodeWords ")"
+;
+
+CodeBlockRef:
+  /'|&/"(" words+=CodeWords ")"
+;
+
+CodeWords:
+  Data | CodeWord | CodeWordWRef | CodeWordSpecial | CodeBlock | CodeBlockRef
+;
+
+CodeWordModifyer:
+  '+' | '-' | '*' | '&' | '=' | '?' | '!' | '$' | '@' | '%' | '^'
+;
+
+CodeWordSpecial:
+  '++' | '--' | '==' | '<=' | '=>' | '<' | '>' | '===' | '+++' | '---'
+;
+
+CodeWordRef:
+  '::' | '->' | '<-'
+;
+
+CodeWord:
+  prefix=CodeWordModifyer? word=ID? suffix=CodeWordModifyer? param=CurryParam?
+;
+
+CodeWordWRef:
+  ':' base=ID CodeWordRef word=ID
+;
+
+CurryParam:
+  "@" param=Data
+;
+
+Comment:
+  /\/\*.*\*\/|\/\/.*$/
+;
+"""
+
